37262d8f5deff8b8561954502602cbbdc70595c8
cpsughrue <cpsughrue@gmail.com>
2024-12-04 14:41:24 -0500

    modify load.user to take verifier id as an arg

--- bpf-progs/load.user.c
+++ bpf-progs/load.user.c
@@ -13,19 +13,25 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
+    if (argc < 3) {
         printf("Not enough args\n");
-        printf("Expected: ./load.user bpf_file bpf_prog_name\n");
+        printf("Expected: ./load.user bpf_file bpf_prog_name [optional:verifier_name]\n");
         return -1;
     }
 
-    char * bpf_path = argv[1];
-    char * prog_name = argv[2];
+    char *bpf_path = argv[1];
+    char *prog_name = argv[2];
+
+    __u32 verifier_type = 0;
+    if (argc == 4) {
+        verifier_type = atoi(argv[3]);
+    }
+
+    printf("Loading program with verifier type: %d\n", verifier_type);
 
     // returns a bpf_object
     struct bpf_object *prog = bpf_object__open(bpf_path);
 
-    __u32 verifier_type = 1;
     // set verifier type inside the prog, later passed on as attr
     bpf_object__set_verifier_type(prog, verifier_type);
     
@@ -38,16 +44,16 @@ int main(int argc, char *argv[])
     struct bpf_program * program = bpf_object__find_program_by_name(prog, prog_name);
 
     if (program == NULL) {
-        printf("Shared 1 failed\n");
+        printf("Could not find pbf_prog_name\n");
         return 0;
     }
 
     printf("PID: %d\n", getpid());
 
-    getchar();
-
     bpf_program__attach(program);
 
+    // While this while loop is running program is attached. To unattach the
+    // program kill ./load.user with Ctrl+C
     while (1) {
         sleep(1);
     }
